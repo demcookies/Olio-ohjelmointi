590ce70781802f3d210a52f9e0e3258da817c195
demcookies <c@demcooki.es>
2018-01-25 09:35:01 +0200

    Add project files.

---
+++ Olio-ohjelmointi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_ohjelmointi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
