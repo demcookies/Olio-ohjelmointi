f1fa2d285967f7118c175f2908e07d4e821d6c62
demcookies <c@demcooki.es>
2018-01-25 11:00:10 +0200

    Added new CarApplicaton -solution

---
+++ CarApplication/CarApplication/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        //Properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int Speed { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public void Accelerate()
+        {
+            Speed += 5;
+        }
+        public void Brake()
+        {
+            Speed -= 5;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("CAR DATA:");
+            Console.WriteLine("model:" + Model);
+            Console.WriteLine("color:" + Color);
+            Console.WriteLine("engine:" + Engine);
+            Console.WriteLine("speed:" + Speed);
+            Console.WriteLine("fuzzy dices:" + FuzzyDices);
+        }
+    }
+}
---
+++ CarApplication/CarApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car cart = new Car();
+            cart.Color = "Shitty brown";
+            cart.Engine = 1;
+            cart.FuzzyDices = false;
+            cart.Model = "BandwaGONE";
+            cart.PrintData();
+            Console.ReadLine();
+            cart.Accelerate();
+            cart.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
