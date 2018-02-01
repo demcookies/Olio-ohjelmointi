075b4c56c91ab3e0af558ecb2e5ab65adcbd66b2
demcookies <c@demcooki.es>
2018-02-01 10:50:58 +0200

    Week 5 Elevator solution initialized

---
+++ W5/W5/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5
+{
+    class Elevator
+    {
+        private int floor = 1;
+
+        public int Floor
+        {
+            get { return floor; }
+            set
+            {
+                if (value < 1) { Console.WriteLine("Elevator can't go underground!"); }
+                else if (value > 5) { Console.WriteLine("Elevator can't go trough the roof!"); }
+                else { floor = value; }
+            }
+        }
+
+    }
+}
---
+++ W5/W5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator hissi = new Elevator();
+            Console.WriteLine("Elevator is on the floor: " + hissi.Floor);
+
+            while (true)
+            {
+                Console.Write("To which floor (1-5) you would like to go? ");
+                string floorNumber = Console.ReadLine();
+                int floorNumberInt = 0;
+                Int32.TryParse(floorNumber, out floorNumberInt);
+                hissi.Floor = floorNumberInt;
+                Console.Write("\n");
+                Console.WriteLine("Elevator is on the floor: " + hissi.Floor);
+            }
+        }
+    }
+}
